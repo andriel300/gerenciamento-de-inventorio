14efbcadc9a2fc036c29847a1529107d551faded
Andriel Jose <andrieldata@proton.me>
2024-07-29 18:35:01 -0300

    fix messup divs and centering the divs

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -8,6 +8,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center w-full mb-7">
       {/* LADO ESQUERDO */}
+
       <div className="flex justify-between items-center gap-5">
         <button
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
@@ -15,18 +16,18 @@ const Navbar = () => {
         >
           <Menu className="w-4 h-4" />
         </button>
-      </div>
-      <div className="relative">
-        <input
-          type="search"
-          placeholder="Pesquisar grupos e produtos"
-          className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
-        />
-
-        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
-          <Bell className="text-gray-500" size={20} />
+        <div className="relative">
+          <input
+            type="search"
+            placeholder="Pesquisar grupos e produtos"
+            className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+          />
+          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
+            <Bell className="text-gray-500" size={20} />
+          </div>
         </div>
       </div>
+
       {/* LADO DIREITO */}
 
       <div className="flex justify-between items-center gap-5">
@@ -43,16 +44,16 @@ const Navbar = () => {
               3
             </span>
           </div>
-        </div>
-        <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
-        <div className="flex items-center gap-3 cursor-pointer">
-          <div className="w-9 h-9">image</div>
-          <span className="font-semibold">Andriel J.</span>
+          <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
+          <div className="flex items-center gap-3 cursor-pointer">
+            <div className="w-9 h-9">image</div>
+            <span className="font-semibold">Andriel J.</span>
+            <Link href="/settings">
+              <Settings className="cursor-pointer text-gray-500" />
+            </Link>
+          </div>
         </div>
       </div>
-      <Link href="/settings">
-        <Settings className="cursor-pointer text-gray-500" />
-      </Link>
     </div>
   );
 };
