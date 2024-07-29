b86ba1f3e3c74eea00e3314f06a625f7c164f3a6
Andriel Jose <andrieldata@proton.me>
2024-07-29 20:38:02 -0300

    Add redux-persist for state persistence and enhance Navbar and Sidebar components with toggle functionality

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,10 +1,21 @@
 "use client";
 
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
 import { Bell, Menu, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const isSideBarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSideBarCollapsed));
+  };
+
   return (
     <div className="flex justify-between items-center w-full mb-7">
       {/* LADO ESQUERDO */}
@@ -12,7 +23,7 @@ const Navbar = () => {
       <div className="flex justify-between items-center gap-5">
         <button
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          onClick={() => {}}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
